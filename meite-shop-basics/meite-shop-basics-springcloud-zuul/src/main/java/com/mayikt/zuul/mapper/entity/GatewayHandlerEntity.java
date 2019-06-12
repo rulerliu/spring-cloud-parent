3fb3d31c511c97952f5558b0161cde63e449e3fb
liuwq <liuwq@tydic.com>
2019-06-12 15:39:31 +0800

    'nothing'

--- meite-shop-basics/meite-shop-basics-springcloud-zuul/src/main/java/com/mayikt/zuul/mapper/entity/GatewayHandlerEntity.java
+++ meite-shop-basics/meite-shop-basics-springcloud-zuul/src/main/java/com/mayikt/zuul/mapper/entity/GatewayHandlerEntity.java
@@ -11,8 +11,7 @@ import java.util.Date;
  * GatewayHandler
  * 
  * @description:
- * @author: 97后互联网架构师-余胜军
- * @contact: QQ644064779、微信yushengjun644 www.mayikt.com
+ * @author: liuwq
  * @date: 2019年1月3日 下午3:03:17
  * @version V1.0
  * @Copyright 该项目“基于SpringCloud2.x构建微服务电商项目”由每特教育|蚂蚁课堂版权所有，未经过允许的情况下，
--- meite-shop-service-impl/meite-shop-service-integral/src/main/java/com/mayikt/integral/consume/IntegralConsumer.java
+++ meite-shop-service-impl/meite-shop-service-integral/src/main/java/com/mayikt/integral/consume/IntegralConsumer.java
@@ -17,12 +17,8 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
- * 
- * 
- * 
  * @description: 积分服务消费者
- * @author: 97后互联网架构师-余胜军
- * @contact: QQ644064779、微信yushengjun644 www.mayikt.com
+ * @author: liuwq
  * @date: 2019年1月3日 下午3:03:17
  * @version V1.0
  * @Copyright 该项目“基于SpringCloud2.x构建微服务电商项目”由每特教育|蚂蚁课堂版权所有，未经过允许的情况下，
